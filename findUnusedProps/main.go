b71987b01e9b3005813f9000744d9257d30e8753
robin <robin.seyr@gmx.net>
2017-08-03 13:10:25 +0200

    added in directory searching  (working)

--- findUnusedProps/main.go
+++ findUnusedProps/main.go
@@ -4,17 +4,34 @@ import (
     "fmt"
     "../findUnusedProps/services"
     "os"
+    "flag"
+    "path/filepath"
 )
 
-func main() {
+var paths []string
+
+func visit(path string, f os.FileInfo, err error) error {
+  if filepath.Ext(path) == ".html"{
+    paths = append(paths, path)
+  }
+  return nil
+} 
+
+func main(){
+    flag.Parse()
+    root := flag.Arg(1)
+    filepath.Walk(root, visit)
+
     pathToInputLines := os.Args[1]
-    pathForSearching := os.Args[2]
 
     keys := service.GetKeys(pathToInputLines)
-    fmt.Printf("Keys: %d\n", len(keys))
-    
-    unusedKeys := service.GetUnusedKeys(pathForSearching, keys)
-    fmt.Printf("Unused keys: %d \n", len(unusedKeys))
 
-    service.WriteSliceToFile(unusedKeys)
+    fmt.Printf("Keys: %d\n", len(keys))
+    for i := 0; i < len(paths); i++ { 
+        keys = service.GetUnusedKeys(paths[i], keys)
+    }
+    fmt.Printf("Unused keys: %d \n", len(keys))
+
+    service.WriteSliceToFile(keys)
 }
+
